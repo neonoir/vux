097ecd48266db3a171bc49a71c8a06b64f261068
Sina Samavati <sina.samv@gmail.com>
2014-03-04 22:05:35 +0330

    first draft

--- src/vux_object.erl
+++ src/vux_object.erl
@@ -2,15 +2,38 @@
 
 -export([world_object_loop/7]).
 
-world_object_loop(SubChannel, PubChannel, X, Y, Cycle, MaxX, MaxY) ->
+init({Channel, WorldManagerQ, WorldObjectExchange}, X, Y, Cycle, MaxX, MaxY) ->
+    Sub = #'basic.consume'{queue = WorldManagerQ},
+    #'basic.consume_ok'{consumer_tag = Tag} = amqp_channel:subscribe(Channel, Sub, self()),
+    world_object_loop({Channel, WorldManagerQ, WorldObjectExchange}, X, Y, Cycle, MaxX, MaxY).
+
+world_object_loop({Channel, WorldManagerQ, WorldObjectExchange}, X, Y, Cycle, MaxX, MaxY) ->
     receive
-        {SubChannel, StateList} ->
-            {X1, Y1} = calculate_state(StateList, X, Y, MaxX, MaxY),
-            publish_state(PubChannel, {self(), X1, Y1, Cycle + 1}),
-            world_object_loop(SubChannel, PubChannel, X1, Y1, Cycle + 1, MaxX, MaxY);
-        _ ->
-            world_object_loop(SubChannel, PubChannel, X, Y, Cycle, MaxX, MaxY)
+        %% This is the first message received
+        #'basic.consume_ok'{} ->
+            world_object_loop({Channel, WorldManagerQ, WorldObjectExchange}, X, Y, Cycle, MaxX, MaxY);
+        %% This is received when the subscription is cancelled
+        #'basic.cancel_ok'{} ->
+            cancel_ok;
+        %% A delivery
+        {#'basic.deliver'{delivery_tag = Tag}, WorldStateList } ->
+            {X1, Y1} = calculate_state(WorldStateList, X, Y, MaxX, MaxY),
+            amqp_channel:cast(Channel, WorldObjectExchange, #amqp_msg{payload = {self(), X1, Y1, Cycle+1}}),
+
+            %% Ack the message
+            amqp_channel:cast(Channel, #'basic.ack'{delivery_tag = Tag}),
+
+            %% Loop
+            world_object_loop({Channel, WorldManagerQ, WorldObjectExchange}, X, Y, Cycle+1, MaxX, MaxY)
     end.
+    %% receive
+    %%     {SubChannel, StateList} ->
+    %%         {X1, Y1} = calculate_state(StateList, X, Y, MaxX, MaxY),
+    %%         publish_state(PubChannel, {self(), X1, Y1, Cycle + 1}),
+    %%         world_object_loop(SubChannel, PubChannel, X1, Y1, Cycle + 1, MaxX, MaxY);
+    %%     _ ->
+    %%         world_object_loop(SubChannel, PubChannel, X, Y, Cycle, MaxX, MaxY)
+    %% end.
 
 calculate_state(_, MaxX, MaxY, MaxX, MaxY) ->
     {MaxX, MaxY};
@@ -27,4 +50,4 @@ calculate_state(StateList, X, Y, MaxX, MaxY) ->
             Y1 =< -MaxY -> -MaxY;
             true -> Y1
          end,
-    {X2, Y2a}.
+    {X2, Y2}.
--- src/vux_spawn.erl
+++ src/vux_spawn.erl
@@ -3,10 +3,79 @@
 -export([world_object_spawn/1]).
 -export([world_object_spawn/2]).
 
+-include_lib("amqp_client/include/amqp_client.hrl").
+
+
 start(MaxX, MaxY, N) ->
     StateList = generate_state_list(MaxX, MaxY, N),
-    InitialStateList = world_object_spawn(StateList, {MaxX, MaxY}),
-    publish_state_list(InitialStateList).
+
+
+    {ok, Connection} = amqp_connection:start(#amqp_params_network{}),
+    {ok, Channel} = amqp_connection:open_channel(Connection),
+    %% Declare a queue
+    #'queue.declare_ok'{queue = WorldManagerQ} =
+        amqp_channel:call(Channel, #'queue.declare'{queue = <<"world_manager_queue">>}),
+    WorldManagerExchange = #'basic.publish'{exchange = <<"world_manager_exchange">>, routing_key = WorldManagerQ},
+
+    #'queue.declare_ok'{queue = WorldObjectQ} =
+        amqp_channel:call(Channel, #'queue.declare'{queue = <<"world_object_queue">>}),
+    WorldObjectExchange = #'basic.publish'{exchange = <<"world_object_exchange">>, routing_key = WorldObjectQ},
+
+    %% amqp_channel:cast(Channel, Publish, #amqp_msg{payload = StateList}),
+
+    WOPubSubInfo = {Channel, WorldManagerQ, WorldObjectExchange},
+    WMPubSubInfo = {Channel, WorldObjectQ, WorldManagerExchange},
+
+    InitialStateList = world_object_spawn(WOPubSubInfo, StateList, {MaxX, MaxY}),
+    world_manager_loop( WMPubSubInfo, InitialStateList, N).
+
+
+
+
+world_object_spawn(PubSubInfo, StateList, {MaxX, MaxY}) ->
+    [world_object_spawn(PubSubInfo, X, Y, {MaxX, MaxY}) || {X, Y} <- StateList].
+
+world_object_spawn(PubSubInfo, X, Y, {MaxX, MaxY}) ->
+    Pid = spawn(vux_object, init, [PubSubInfo, X, Y, 0, MaxX, MaxY]),
+
+    {Pid, X, Y, 0}.
+
+
+
+
+world_manager({Channel, WorldObjectQ, WorldManagerExchange}, StateList, N) ->
+    amqp_channel:cast(Channel, WorldManagerExchange, #amqp_msg{payload = StateList}),
+    Sub = #'basic.consume'{queue = WorldObjectQ},
+    #'basic.consume_ok'{consumer_tag = Tag} = amqp_channel:subscribe(Channel, Sub, self()),
+    world_manager_loop(Channel, WorldManagerExchange, [], N, N).
+
+world_manager_loop(Channel,  WorldManagerExchange, WorldObjectStateList,  N, 0) ->
+    %% SAVE STATE TO DB
+    io:format("~p~n", [WorldObjectStateList]),
+    amqp_channel:cast(Channel, WorldManagerExchange, #amqp_msg{payload = WorldObjectStateList}),
+    world_manager_loop(Channel, WorldManagerExchange, [], N, N).
+
+world_manager_loop(Channel,  WorldManagerExchange, WorldObjectStateList, N, Count) ->
+    receive
+        %% This is the first message received
+        #'basic.consume_ok'{} ->
+            world_manager_loop(Channel, WorldManagerExchange, WorldObjectStateList, N, Count);
+        %% This is received when the subscription is cancelled
+        #'basic.cancel_ok'{} ->
+            cancel_ok;
+        %% A delivery
+        {#'basic.deliver'{delivery_tag = Tag}, WorldObjectState } ->
+            WorldObjectStateList2 = [WorldObjectState | WorldObjectStateList],
+
+            %% Ack the message
+            amqp_channel:cast(Channel, #'basic.ack'{delivery_tag = Tag}),
+
+            %% Loop
+            world_manager_loop(Channel, WorldManagerExchange, WorldObjectStateList2, N, Count-1)
+    end.
+
+
+
 
 generate_state_list(MaxX, MaxY, N) ->
     %% [{X, Y}]
@@ -18,10 +87,3 @@ generate_state_list(MaxX, MaxY, N, Acc) ->
     X = crypto:rand_uniform(-MaxX, MaxX),
     Y = crypto:rand_uniform(-MaxY, MaxY),
     generate_state_list(MaxX, MaxY, N - 1, [{X, Y}|Acc]).
-
-world_object_spawn(StateList, {MaxX, MaxY}) ->
-    [world_object_spawn(X, Y, {MaxX, MaxY}) || {X, Y} <- StateList].
-
-world_object_spawn(X, Y, {MaxX, MaxY}) ->
-    Pid = spawn(vux_object, world_object_loop, [SubChannel, PubChannel, X, Y, 0, MaxX, MaxY]),
-    {Pid, X, Y, 0}.
